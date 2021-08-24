7ec0e5cc269cef10adf71d0886b1858f01b79798
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-24 17:58:22 +0800

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-bin/rails app:db:prepare
+bin/rails db:prepare
 bin/rails g rails_com:migrations -f
-bin/rails app:db:migrate
-rm -f test/dummy/tmp/pids/server.pid
+bin/rails db:migrate
+rm -f tmp/pids/server.pid
 bin/rails s -b 0.0.0.0
