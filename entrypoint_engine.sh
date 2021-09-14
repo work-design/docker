0ccacffa1d6b8f051fdff07d519892386e2e2823
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-14 14:59:05 +0800

    chagne default to rails extend migration

--- entrypoint_engine.sh
+++ entrypoint_engine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 bin/rails app:db:prepare
-bin/rails g rails_com:migrations -f
+bin/rails g rails_extend:migrations -f
 bin/rails app:db:migrate
 rm -f test/dummy/tmp/pids/server.pid
 bin/rails s -b 0.0.0.0
